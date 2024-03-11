db4f05b6a718271ba50adc778709ad0594a814d9
afelka <erdememin@gmail.com>
2024-03-11 09:46:50 +0100

    create box office vs goodreads plot

--- compare_two_datasets.R
+++ compare_two_datasets.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(tidyr)
 library(fuzzyjoin)
+library(ggplot2)
+library(ggimage)
+library(ggrepel)
 
 movies <- read.csv("movies_with_highest_box_office.csv")
 books <- read.csv("goodreads_books_with_many_ratings.csv")
@@ -33,5 +36,37 @@ books_combined <- books %>% inner_join(books_fuzzy_join, by = "book_names_shorte
                   left_join(movies %>% select(movie_title_shortened, worldwide_gross, rating), 
                             by = "movie_title_shortened")
 
+relative_path <- file.path("..", "goodreads_books_with_many_ratings")
 
+books_combined$image_name <- file.path(relative_path, books_combined$image_name)
+
+highest_no_of_rating <- books_combined %>% arrange(desc(no_of_ratings)) %>% slice(1)
+highest_box_office <- books_combined %>% arrange(desc(worldwide_gross)) %>% slice(1)
+highest_box_office_under_twoandhalf_goodreads <- books_combined %>% filter(no_of_ratings <= 2500000) %>% arrange(desc(worldwide_gross)) %>% slice(1)
+
+
+# Create a ggplot2 plot with movie posters as points
+gg <- ggplot(books_combined, aes(x = no_of_ratings, y = worldwide_gross)) +
+  geom_image(aes(image = image_name), size = 0.03) +  # Add book covers 
+  geom_text_repel(data = highest_no_of_rating, aes(x = no_of_ratings, y = worldwide_gross,
+                                                 label = paste0(book_names, "\nhas the highest number of goodreads ratings with \n",
+                                                                scales::comma(no_of_ratings))),
+                  color = "red", size = 3 , vjust = 2.0, hjust = 0.7) +
+  geom_text_repel(data = highest_box_office, aes(x = no_of_ratings, y = worldwide_gross,
+                                                 label = paste0(book_names,  "\nhas the highest box office revenue with \n$",
+                                                                scales::comma(worldwide_gross))),
+                  color = "darkblue", size = 3  , vjust = 1.75) +
+  geom_text_repel(data = highest_box_office_under_twoandhalf_goodreads, aes(x = no_of_ratings, y = worldwide_gross,
+                                                label = paste0(book_names,  "\nhas the highest box office revenue within \nthe books under 2.5 million Goodreads votes with \n$",
+                                                          scales::comma(worldwide_gross))),
+                  color = "purple", size = 3 , vjust = -1.0, hjust = 0.5) +
+  labs(title = "Number of Goodreads Ratings vs Box Office",
+       x = "Number of Goodreads Ratings",
+       y = "Box Office in $") +
+  scale_y_continuous(labels = scales::comma) +  
+  scale_x_continuous(labels = scales::comma) +  
+  theme_minimal()  
+
+# Save the plot as an image file
+ggsave( "box_office_vs_goodreads.png",plot = gg, bg="white")
 
